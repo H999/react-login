b8c8690315b9579e385eb0a54bc008a79f5dd875
H999 <32168478+H999@users.noreply.github.com>
2019-09-28 10:16:39 +0700

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, FormLabel, Form, Row, Col } from "react-bootstrap";
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 import "./Login.css";
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 export default class Login extends Component {
   constructor(props) {
@@ -24,12 +33,13 @@ export default class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
+    alert("email: " + this.state.email + "\npass: " + this.state.password);
   }
 
   render() {
     return (
       <div className="Login">
-        <form onSubmit={this.handleSubmit}>
+        {/* <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="email" bsSize="large">
             <FormLabel>Email</FormLabel>
             <FormControl
@@ -55,7 +65,65 @@ export default class Login extends Component {
           >
             Login
           </Button>
-        </form>
+        </form> */}
+        <Form onSubmit={this.handleSubmit}>
+          <img className="logo" src={"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"} />
+          <Form.Group controlId="email" bsSize="large">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="email@examle" />
+          </Form.Group>
+          <Form.Group controlId="password" bsSize="large">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              placeholder="Password" />
+          </Form.Group>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </Form>
+
+        <div>
+          <FacebookLogin
+            appId="1088597931155576"
+            fields="name,email,picture"
+            icon={<img
+              margin-right="10px"
+              src={"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/267px-F_icon.svg.png"}
+              width="38" />}
+            cssClass="fbtn"
+            textButton="&nbsp;&nbsp;&nbsp;Login with Facebook"
+            // onClick={componentClicked}
+            callback={responseFacebook}
+          />
+        </div>
+        <div>
+          <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            icon="True"
+            theme="dark"
+            className="Button g"
+            // stlye={{font:Helvetica;font-weight:700; }}
+            // render={renderProps => (
+            //   <button onClick={renderProps.onClick} disabled={renderProps.disabled}></button>
+            // )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          /></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Login from './Login';
-import { GoogleLogin } from 'react-google-login';
-import FacebookLogin from 'react-facebook-login';
- 
-//ReactDOM.render(<App />, document.getElementById('root'));
+// import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Login />, document.getElementById('login'));
 
-const responseFacebook = (response) => {
-  console.log(response);
-}
- 
-ReactDOM.render(
-  <FacebookLogin
-//   clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-  buttonText="Login Facebook"
-  onSuccess={responseFacebook}
-  onFailure={responseFacebook}
-  cookiePolicy={'single_host_origin'}
-//   cssClass="my-facebook-button-class"
-  icon="fa-facebook"
-    appId="1088597931155576"
-    fields="name,email,picture"
-    // callback={responseFacebook} 
-    />,
-  document.getElementById('fbButton')
-);
-
-const responseGoogle = (response) => {
-    console.log(response);
-  }
-   
-  ReactDOM.render(
-    <GoogleLogin
-      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-      buttonText="Login Google"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={'single_host_origin'}
-    />,
-    document.getElementById('gButton')
-  );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
